45ce270ffcc05a9c96821495b114521f9da2f3ba
Murilo Sousa <murilosz085@gmail.com>
2026-04-24 15:19:56 -0300

    desenvolvimento das classes Robo e RoboInteligente

--- src/model/Robo.java
+++ src/model/Robo.java
@@ -1,15 +1,23 @@
 package model;
 
+import exceptions.MovimentoInvalidoException;
+
 public class Robo {
     private int eixoX;
     private int eixoY;
+    private String cor;
 
     public Robo() {
     }
 
-    public Robo(int eixoY, int eixoX) {
+    public Robo(String cor) {
+        this.cor = cor;
         this.eixoY = 0;
         this.eixoX = 0;
+    }
+
+    public String getCor() {
+        return cor;
     }
 
     public int getEixoX() {
@@ -27,4 +35,46 @@ public class Robo {
     public void setEixoY(int eixoY) {
         this.eixoY = eixoY;
     }
+
+    public void mover(String direcao) throws MovimentoInvalidoException{
+        int novoX = this.eixoX;
+        int novoY = this.eixoY;
+
+        switch (direcao.toLowerCase()){
+            case "up": novoY++; break;
+            case "down": novoY--; break;
+            case "right": novoX++; break;
+            case "left": novoX--; break;
+            default:
+                throw new MovimentoInvalidoException("Comando" + direcao + "desconhecido");
+        }
+
+        validarPosicao(novoX, novoY, direcao);
+
+        this.eixoX = novoX;
+        this.eixoY = novoY;
+        System.out.println("O robo" + cor + "moveu-se para ("+ eixoX +"," + eixoY + ")");
+    }
+
+    public void mover(int direcao) throws MovimentoInvalidoException{
+        switch(direcao){
+            case 1: mover("up"); break;
+            case 2: mover("down"); break;
+            case 3: mover("right"); break;
+            case 4: mover("left"); break;
+            default:
+                throw new MovimentoInvalidoException("Código" + direcao + "é inválido (use 1 a 4)");
+        }
+    }
+
+    public boolean encontrouAlimento(int alimentoX, int alimentoY){
+        return (this.eixoX == alimentoX && this.eixoY == alimentoY);
+    }
+
+    public void validarPosicao(int x, int y, String direcao) throws MovimentoInvalidoException{
+        if (eixoX < 0 || eixoY < 0){
+            throw new MovimentoInvalidoException("Movimento inválido para" + direcao + "O robo pode não entrar em zonas negativas");
+        }
+    }
+
 }
--- src/model/RoboInteligente.java
+++ src/model/RoboInteligente.java
@@ -1,4 +1,42 @@
 package model;
 
+import exceptions.MovimentoInvalidoException;
+import java.util.Random;
+
 public class RoboInteligente extends Robo {
+
+    private Random random;
+
+    public RoboInteligente(String cor, Random random) {
+        super(cor);
+        this.random = new Random();
+    }
+
+    @Override
+    public void mover(String direcao){
+        boolean movido = false;
+        String direcaoTentativa = direcao;
+
+        while(!movido){
+            try {
+                super.mover(direcaoTentativa);
+                movido = true;
+            }catch (MovimentoInvalidoException e){
+                System.out.println("Robo Inteligente detectou erro: " + e.getMessage() + "Tentando nova direção");
+
+                direcaoTentativa = gerarDirecaoAleatoria();
+            }
+        }
+    }
+
+    private String gerarDirecaoAleatoria(){
+        int opcao = random.nextInt(4) + 1;
+        switch (opcao){
+            case 1: return "up";
+            case 2: return "down";
+            case 3: return "right";
+            case 4: return  "left";
+            default: return "up";
+        }
+    }
 }
