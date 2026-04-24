ce5e0e5fb5bd2b226d6fd040a1ea920092e81cd4
murilosousaz <murilosz085@gmail.com>
2026-04-24 09:42:54 -0300

    criação de construtor e getter and setters do robo

--- src/entities/Robo.java
+++ src/entities/Robo.java
@@ -1,5 +1,30 @@
 package entities;
 
 public class Robo {
+    private int eixoX;
+    private int eixoY;
 
+    public Robo() {
+    }
+
+    public Robo(int eixoY, int eixoX) {
+        this.eixoY = 0;
+        this.eixoX = 0;
+    }
+
+    public int getEixoX() {
+        return eixoX;
+    }
+
+    public void setEixoX(int eixoX) {
+        this.eixoX = eixoX;
+    }
+
+    public int getEixoY() {
+        return eixoY;
+    }
+
+    public void setEixoY(int eixoY) {
+        this.eixoY = eixoY;
+    }
 }
