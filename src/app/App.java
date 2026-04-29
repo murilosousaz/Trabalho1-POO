0ba6c25500c39b88515712a998dba11c5337d1ee
murilosousaz <murilosz085@gmail.com>
2026-04-29 10:19:37 -0300

    add do App.java

--- src/app/App.java
+++ src/app/App.java
@@ -1,4 +1,36 @@
 package app;
 
-public class App {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("/fxml/menu.fxml")
+        );
+        Parent raiz = loader.load();
+
+        Scene scene = new Scene(raiz, 400, 560);
+
+        // Configurações da janela
+        stage.setTitle("Robô Game — PET Computação UECE");
+        stage.setScene(scene);
+
+        stage.setMinWidth(400);
+        stage.setMinHeight(560);
+
+        stage.centerOnScreen();
+
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
